e45cd3103b720ed9c0529c2907200e4e62ded5ef
Riya <meriyaverma02@gmail.com>
2023-06-24 23:20:01 +0530

    Updates

---
+++ precache-manifest.2bacb25f91750d837c8d27065b344455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45faaac95229ec628b224e7becd64115",
+    "url": "/searchImageApp/index.html"
+  },
+  {
+    "revision": "946c085b314db2787151",
+    "url": "/searchImageApp/static/css/main.8f084d8d.chunk.css"
+  },
+  {
+    "revision": "6b931627fd1cae750f12",
+    "url": "/searchImageApp/static/js/2.85de5a00.chunk.js"
+  },
+  {
+    "revision": "fa29804b7cfd24548f33a1a01149f0e9",
+    "url": "/searchImageApp/static/js/2.85de5a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946c085b314db2787151",
+    "url": "/searchImageApp/static/js/main.1bb8c9dc.chunk.js"
+  },
+  {
+    "revision": "ab424cdf2965a6692e95",
+    "url": "/searchImageApp/static/js/runtime-main.97ca3491.js"
+  }
+]);
